c55b1594da2861ed02f478f6ad06c86a891eb310
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-17 20:05:11 +1000

    e

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
-﻿const Discord=require('discord.js');
-const config=require('./config.json')
-const command=require('./command');
-const { title } = require('process');
-require('dotenv').config();
-const client=new Discord.Client()
-//Handlers
-client.on('ready', ready=>{
-    console.log('Logged into bot')
-    client.user.setPresence({
-        activity: {
-            status:'online',
-            name:'yo-ooyo',
-            type:'Watching',
-         
-        }}).then('Presence Set') 
-    command(client,'ping', (message)=>{
-        var yourping = new Date().getTime() - message.createdTimestamp
-        var botping = Math.round(client.ws.ping)
-        let embed=new Discord.MessageEmbed()
-        .setTitle('PONG')
-        .addFields(
-            {name:'My Ping', value:botping},
-            {name:'Your Ping', value:yourping},)
-        .setColor('#f54f')
-                   
-    
-                
-                            
+﻿//Importing all needed Commands
+const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
+require('dotenv').config()
+let token=process.env.token
+const colors = require("colors"); //this Package is used, to change the colors of our Console! (optional and doesnt effect performance)
+const fs = require("fs"); //this package is for reading files and getting their inputs
 
-        //message.channel.send(`PONG! Your ping: ${yourping} \nBots ping: ${botping}`)
-        message.channel.send(embed)
-            
+//Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
+const client = new Discord.Client({
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  disableEveryone: true,
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+});
 
-                
-            
-        
-    })
-    
+//Client variables to use everywhere
+client.commands = new Discord.Collection(); //an collection (like a digital map(database)) for all your commands
+client.aliases = new Discord.Collection(); //an collection for all your command-aliases
+client.categories = fs.readdirSync("./commands/"); //categories
+client.cooldowns = new Discord.Collection(); //an collection for cooldown commands of each user
 
-        
+//Loading files, with the client variable like Command Handler, Event Handler, ...
+["command", "events"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+//login into the bot
+client.login(require("./botconfig/config.json").token);
 
-   
-})
-
- client.login(process.env.TOKEN)
-
- /*client.once('ready', () => {
-    client.user.setPresence({
-        activity: {
-            status: 'online',
-            name: 'yo-ooyo coding',
-            type: 'WATCHING'*/
+/** Template by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template */
