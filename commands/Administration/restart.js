2b14dc99a97d35886e8a859aef413047bbb896d5
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-20 08:54:47 +1000

    fix restart

--- commands/Administration/restart.js
+++ commands/Administration/restart.js
@@ -1,6 +1,7 @@
 const {spawn}=require('child_process')
 module.exports = {
     name: "restart",
+    aliases:['reboot']
     description: "exclusive command only for creator",
     category: "Administration",
     memberpermissions:"VIEW_CHANNEL",
@@ -9,16 +10,21 @@ module.exports = {
     run:async(client, message, args)=>{
         if(!message.author.id===827388013062389761)return message.channel.send("No, don't even try")
         message.channel.send('Are you sure?')
-        if(message.content.startsWith('no')) return
-        else{ 
-            //Need to push!
-            message.reply("Restarting")
-            const process=spawn('python', ['../../python/restart.py'])
-            process.stdout.on('data', data=>{
-            console.log(data.toString())
+        client.on('message',(msg)=>{
+            if(!msg.author.id===827388013062389761)return
+            if(msg.content.startsWith(no))return message.channel.send('Cancelled')
+            else{
+
+                 message.reply("Restarting")
+                 const process=spawn('python', ['../../python/restart.py'])
+                 process.stdout.on('data', data=>{
+                     console.log(data.toString())
+                      })}
     
 
-    })}
+   
+            
+        })
 
     },
 };
