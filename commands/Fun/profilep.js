d84bf00ed2dbc910a139cabd863aef1554f18dbf
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-19 11:19:36 +1000

    bugfix

--- commands/Fun/profilep.js
+++ commands/Fun/profilep.js
@@ -9,12 +9,12 @@ module.exports = {
     run : async(client, message, args)=>{
         let embed=new Discord.MessageEmbed().setColor('RANDOM')
         if (!args[0]){
-            embed.setImage(message.author.displayAvatarURL())
+            embed.setImage(message.author.displayAvatarURL()).setTitle(`${message.author.username}'s Profile Image`)
             message.channel.send(embed)
 
         }else if(args[0]){
             let user=message.mentions.users.first()
-            embed.setImage(user.displayAvatarURL())
+            embed.setImage(user.displayAvatarURL()).setTitle(`${user}'s Profile Image`)
             message.channel.send(embed)
         }
 
