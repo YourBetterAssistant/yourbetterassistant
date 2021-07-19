a3ec9d1a989c242e62125fa6439989d3507b150e
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-19 14:36:33 +1000

    E

--- commands/Fun/weather.js
+++ commands/Fun/weather.js
@@ -11,16 +11,18 @@ module.exports = {
         if(!args[0])return message.reply('Humph I quit, just tell me the country!')
         let place=args.slice(0).join(" ")
         weather.find({search: place, degreeType: 'C'}, function(err, result) {
-           
+
+            if(err)return message.channel.send(err)
             console.log(result[0].location.name);
             console.log(result[0].current.imageURL)
+
 
  
                 let embed = new Discord.MessageEmbed()
                 .setTitle(`Weather - ${result[0].location.name}`)
                 .setColor("RANDOM")
                 .setDescription("Temperature units can may be differ some time")
-                .addField('Timezone', result[0].location.timezone)
+                .addField('Timezone', `${result[0].location.timezone}`)
                 .addField("Temperature", `${result[0].current.temperature} Celcius`, true)
                 .addField("Sky Text", result[0].current.skytext, true)
                 .addField("Humidity", result[0].current.humidity, true)
@@ -29,7 +31,7 @@ module.exports = {
                 .addField("Wind Display", result[0].current.winddisplay, true)
                 .setThumbnail(result[0].current.imageUrl);
                 message.channel.send(embed)
-                if(err)return message.channel.send(err)
+ 
 
           });
       
