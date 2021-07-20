0fb4f7b70628ce4c303ca7944cfa95b699a3156b
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-20 13:55:55 +1000

    e

--- commands/OWNER/pull.js
+++ commands/OWNER/pull.js
@@ -9,10 +9,22 @@ module.exports = {
     run:async(client, message, args)=>{
         if(!message.author.id==='827388013062389761')
         message.reply("Pulling!")
-        try{shell.exec('git pull&&sudo reboot')
+        try{shell.exec('git pull')
     }catch(err){
         message.channel.send(err)
     }
+        message.lineReply('Do you want me to reboot')
+        client.on('message', msg=>{
+            if(!message.author.id==='827388013062389761')return
+            if(msg.content.startsWith('no')) return message.channel.send('Cancelled')
+            else{
+                shell.exec('sudo reboot')
+            }
+                
+            
+    
+        })
+
         
     },
 };
