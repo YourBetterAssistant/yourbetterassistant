43d24b3756ccd81a6aba47d5a13bd2ccc3cdb54c
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-20 12:30:24 +1000

    reboot fix I think

--- python/restart.py
+++ python/restart.py
@@ -1,2 +1,8 @@
 import os
-os.system('systemctl reboot -i')
+def restart():
+    print("restarting Pi")
+    command = "/usr/bin/sudo /sbin/shutdown -r now"
+    import subprocess
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print(output)
