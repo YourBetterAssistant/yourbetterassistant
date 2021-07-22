b73ea023e3eb3c6f8c1c0c55b7ef093d345c3f6e
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-22 17:49:46 +1000

    Work

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -1,3 +1,5 @@
+const Discord=require('discord.js')
+const mongoCurrency=require('discord-mongo-currency-fork')
 module.exports = {
     name: "work",
     description: "work ",
@@ -5,9 +7,44 @@ module.exports = {
     guildOnly: true,
     memberpermissions:"VIEW_CHANNEL",
     adminPermOverride: true,
-    cooldown: 2,
+    cooldown: 60*60*1,
     usage: "<usage>",
     run:async(client, message, args)=>{
-        message.reply("IN PROGRESS!")
+        message.lineReply("YOU HAVE BEEN AUTOMATICALLY ASSIGNED THE JOB `APPRENTICE COMMONER` MORE JOBS WILL BE ADDED LATER!")
+        let job='APPRENTIC COMMONER'
+        let possibleJobs=['FRIDGE', 'BUY', 'JOBS', 'TV', 'POLITICS', 'LIFE', 'CHILDREN']
+        var item = possibleJobs[Math.floor(Math.random()*possibleJobs.length)];
+        let embed=new Discord.MessageEmbed()
+        .setTitle(`JOB:${job}`)
+        .setDescription('YOU MUST TYPE THIS WORD IN CHAT TO COMPLETER YOUR JOB!')
+        .addField(item)
+        message.channel.send(embed)
+        const randomCoins = Math.floor(Math.random() * 500) + 1;
+        let filter = m => m.author.id === message.author.id
+        message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 30000,
+            errors: ['time']
+          }).then(message=>{
+            message = message.first()
+            if (message.content.startsWith() == item) {
+              message.channel.send(`CORRECT YOU HAVE EARNT 1000YBCs`)
+              mongoCurrency.giveCoins(message.author.id, message.guild.id, 1000)
+              return
+
+            } else {
+              message.channel.send(`WRONG! YOU HAVE EARNT ${randomCoins}YBCs`)}
+              mongoCurrency.giveCoins(message.author.id, message.guild.id, randomCoins)
+              return
+
+          }).catch(collected=>{
+            message.channel.send(`TIMES UP! YOU HAVE EARNT ${randomCoins}YBCs`)
+            mongoCurrency.giveCoins(message.author.id, message.guild.id, randomCoins)
+            return
+
+          })
+        
+
+
     },
 };
