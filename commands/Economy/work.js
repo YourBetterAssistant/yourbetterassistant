9bb26627d76057b47ceb69010ca9b7fa8eaf1381
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-22 14:12:41 +1000

    fix

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -6,7 +6,6 @@ module.exports = {
     memberpermissions:"VIEW_CHANNEL",
     adminPermOverride: true,
     cooldown: 2,
-    args: args,
     usage: "<usage>",
     run:async(client, message, args)=>{
         message.reply("IN PROGRESS!")
