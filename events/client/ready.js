229949b2efa45392c457e71810593935543bab64
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-29 11:18:18 +1000

    ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -60,6 +60,23 @@ module.exports = async client => {
     }
 
   })
+  client.on('guildMemberRemove', async(member) => {
+    const guild = client.guilds.cache.get(member.guild.id);
+    await mongo().then(async mongoose=>{
+      try{
+        let countInfo=await countSchema.findOne({_id:member.guild.id})
+            const vc=countInfo.voiceChannelID
+            setInterval(function () {
+              var memberCount = guild.members.cache.filter(member => !member.user.bot).size;  
+              var memberCountChannel = guild.channels.cache.get(vc);
+              memberCountChannel.setName(`${memberCount} members!`);
+            }, 1000)
+      }finally{
+        mongoose.connection.close()
+      }
+    })
+    
+  });
   client.on('guildMemberAdd', async(member)=>{
     const welcomeSchema=require('../../Schemas/welcomeSchema')
     const logSchema=require('../../Schemas/logSchema')
