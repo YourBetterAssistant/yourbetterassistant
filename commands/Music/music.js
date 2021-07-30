267e11bca83c34ee9696a96794be245463dee31a
NotTimIsReal <alastairrintumathew@gmail.com>
2021-07-30 12:26:02 +1000

    Big fix

--- commands/Music/music.js
+++ commands/Music/music.js
@@ -135,7 +135,7 @@ module.exports = {
     
 }
 const video_player = async (guild, song) => {
-    const song_queue = queue.get(message.guild.id);
+    const song_queue = queue.get(guild.id);
 
     //If no song is left in the server queue. Leave the voice channel and delete the key and value pair from the global queue.
     if (!song) {
