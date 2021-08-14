6bf15037863d6e06589d25e92a183bececfbdfd3
NotTimIsReal <alastairrintumathew@gmail.com>
2021-08-14 20:19:33 +1000

    code

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const config=require('./botconfig/config.json')
 const colors = require("colors"); //this Package is used, to change the colors of our Console! (optional and doesnt effect performance)
 const fs = require("fs"); //this package is for reading files and getting their inputs
 //Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
-const int=new Discord.Intents()
-int.add()
 const client=new Discord.Client({fetchAllMembers: true, messageCacheMaxSize: 10, disableEveryone: false,partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],intents:["GUILDS", "GUILD_MESSAGES",'GUILD_MESSAGE_REACTIONS', 'GUILD_VOICE_STATES', 'GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILD_BANS', 'GUILD_INVITES']})
 /*const disbut = require('discord-buttons');
 disbut(client);*/
