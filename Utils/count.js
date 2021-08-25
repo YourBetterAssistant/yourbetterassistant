58508c3a90634f9b6ca2b0ab1aa66eec670b7601
NotTimIsReal <alastairrintumathew@gmail.com>
2021-08-26 09:08:42 +1000

    fix error

--- Utils/count.js
+++ Utils/count.js
@@ -1,21 +1,20 @@
 const countSchema=require('../Schemas/countSchema')
 const mongo=require('../botconfig/mongo')
 async function count(message){
-        try{
-          await mongo().then(async()=>{
-          let countInfo=await countSchema.findOne({_id:message.guild.id})
-        if(countInfo){
-          const vc=countInfo.voiceChannelID
-          setInterval(function () {
-            var memberCount = message.guild.members.cache.filter(m => !m.user.bot).size;  
-            var memberCountChannel =  message.guild.channels.cache.get(vc);
-            if(!memberCountChannel)return
-            memberCountChannel.setName(`${memberCount} members!`);
-         }, 1000)}}
+  try{        
+    await mongo().then(async()=>{
+    let countInfo=await countSchema.findOne({_id:message.guild.id})
+  if(countInfo){
+    const vc=countInfo.voiceChannelID
+    setInterval(function () {
+      var memberCount = message.guild.members.cache.filter(m => !m.user.bot).size;  
+      var memberCountChannel =  message.guild.channels.cache.get(vc);
+      if(!memberCountChannel)return
+      memberCountChannel.setName(`${memberCount} members!`);
+   }, 1000)}}
 
-        }catch(err){require('../Handlers/errorHandler')}
-        
-        )
+  )}catch(err){require('../handlers/errorHandler')}}
+
         
 exports.count=count
 
