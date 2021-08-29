249856089b49410a4d6b3c1a915b3962d489f32e
NotTimIsReal <alastairrintumathew@gmail.com>
2021-08-29 17:51:39 +1000

    forgot to add new schemas

---
+++ Schemas/chatbot.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const chatBot=mongoose.Schema({
+    guildID:{
+        type:String,
+        required:true
+    },
+    channelID:{
+        type:String,
+        required:true
+    }
+}) 
+
+
+module.exports=mongoose.model('chatBot', chatBot)
