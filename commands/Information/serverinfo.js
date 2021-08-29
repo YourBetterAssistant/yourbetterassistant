b85e04156f47c53bb577a13ca2d5aedaa038efcc
NotTimIsReal <alastairrintumathew@gmail.com>
2021-08-29 19:51:54 +1000

    serverinfo

---
+++ commands/Information/serverinfo.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require("discord.js");
+const { delay } = require("../../handlers/functions");
+
+module.exports = {
+    name: "serverinfo",
+    description: "shows an in-depth result of the server",
+    category: "Information",
+    memberpermissions:"VIEW_CHANNEL",
+    cooldown: 5,
+    usage: "serverinfo",
+    run:async(client, message, args)=>{
+        //https://discord.com/developers/docs/resources/guild#guild-object-guild-features
+        let msg=await message.channel.send('Getting Info...')
+        delay(1000)
+        msg.edit('Results Found! Placing information...')
+        let embed=new MessageEmbed()
+        .setTitle(`Server Info of ${message.guild.name}`)
+        .setColor("DARK_BLUE")
+        .addFields({name:'Members', value:message.guild.memberCount.toString(), inline:true},
+                   {name:'Owner', value:`<@!${message.guild.ownerId}>`, inline:true},
+                   {name:'Humans', value:message.guild.members.cache.filter(m=>!m.user.bot).size.toString(), inline:true},
+                   {name:'Bots', value:message.guild.members.cache.filter(m=>m.user.bot).size.toString(), inline:true},
+                   {name:'Banned Members', value:message.guild.bans.cache.size.toString(), inline:true},
+                   {name:'Roles', value:message.guild.roles.cache.size.toString(), inline:true},
+                   {name:'Rules Channel', value:`<#${message.guild.rulesChannelId}>`, inline:true},
+                   {name:'Emojis', value:message.guild.emojis.cache.size.toString(), inline:true},
+                   {name:'Stickers', value:message.guild.stickers.cache.size.toString(), inline:true},
+                   {name:'Verification Level', value:message.guild.verificationLevel, inline:true},
+                   {name:'Features:', value:'**\n**', inline:false}
+
+                    )
+        let e=1
+        message.guild.features.forEach(i=>embed.addField(`  Feature ${e++}`, `  <a:greentick:881473012162002984> ${i}`, inline=false))
+        msg.edit({embeds:[embed]})
+    },
+};
