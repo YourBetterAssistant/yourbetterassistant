4b52b413a52e0612a57f935bcf763a7c8735d927
NotTimIsReal <alastairrintumathew@gmail.com>
2021-09-05 16:31:08 +1000

    fix

--- Utils/checkChatChannel.js
+++ Utils/checkChatChannel.js
@@ -2,29 +2,29 @@ const axios= require('axios').default
 const chatBot=require('../Schemas/chatbot')
 async function check(message){
     let check=await chatBot.findOne({guildID:message.guild.id})
-    let ch=check.channelID
     if(check){
-    if(message.channel.id===ch){
-        async function urlGet(url){
-            let axios=require('axios').default
-            axios.get(url)
-          .then(function (response) {
-            // handle success
-            let c=message.guild.channels.cache.get(ch)
-            c.send(response.data.message)
-          })
-          .catch(function (error) {
-            // handle error
-            console.log(error);
-          })
-          .then(function () {
-            // always executed
-          });
-            
-        }
-        urlGet(`https://api.affiliateplus.xyz/api/chatbot?message=${message.content}&botname=YourBetterAssistant&ownername=SomeOneElse&user=1`)
-        return
-        
-    }}
+      let ch=check.channelID
+      if(message.channel.id===ch){
+          async function urlGet(url){
+              let axios=require('axios').default
+              axios.get(url)
+            .then(function (response) {
+              // handle success
+              let c=message.guild.channels.cache.get(ch)
+              c.send(response.data.message)
+            })
+            .catch(function (error) {
+              // handle error
+              console.log(error);
+            })
+            .then(function () {
+              // always executed
+            });
+              
+          }
+          urlGet(`https://api.affiliateplus.xyz/api/chatbot?message=${message.content}&botname=YourBetterAssistant&ownername=SomeOneElse&user=1`)
+          return
+          
+      }}
 }
 exports.check=check
