4e1cf010547b94640c6322b4a0294ba24ad4c3f2
NotTimIsReal <alastairrintumathew@gmail.com>
2021-09-20 17:42:17 +1000

    added event guildCreate, guildLeave will come soon

---
+++ events/client/guildCreate.js
@@ -0,0 +1,29 @@
+const Discord=require('discord.js')
+const { getPrettyDate } = require('../../handlers/functions')
+module.exports=async(client, guild)=>{
+    try{}catch(err){return}
+    let possibleChannel=['chatting', 'general', 'chat', 'talk', 'speak', 'idk', 'bot-commands', 'bot', 'commands']
+    let channel=[]
+    let returnTrue
+    let guildChannel='889412487768850432'
+    guild.channels.cache.forEach(c=>{
+        possibleChannel.forEach(ch=>{
+            if(c.name.toLowerCase().includes(ch)){
+                channel.push(c)
+                return returnTrue=true
+            }
+            if(returnTrue==true)return
+        })
+    })
+    
+    let channelRandom = channel[Math.floor(Math.random()*channel.length)];
+    const embed = new Discord.MessageEmbed()
+        .setColor('ORANGE')
+        .setTitle('Hello!')
+        .setDescription('Hi I am YourBetterAssistant I suggest setting me up with my amazing administration commands as without doing that I will be rather useless. My prefix is `b!` so I sugegst using b!help as you first command')
+        .setTimestamp(new Date())
+    
+    channelRandom.send({embeds:[embed]})
+    guildChannel=client.channels.cache.get(guildChannel)
+    guildChannel.send(`New Joined Guild!\n name: ${guild.name} \n id:${guild.id}\ntime:${getPrettyDate()}`)
+}
--- handlers/functions.js
+++ handlers/functions.js
@@ -107,6 +107,27 @@ module.exports = {
     }catch (e){
       console.log(String(e.stack).bgRed)
     }
+  },
+  getPrettyDate: function(){
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var d = new Date();
+    var day = days[d.getDay()];
+    var hr = d.getHours();
+    var min = d.getMinutes();
+    if (min < 10) {
+        min = "0" + min;
+    }
+    var ampm = "am";
+    if( hr > 12 ) {
+        hr -= 12;
+        ampm = "pm";
+    }
+    var date = d.getDate();
+    var month = months[d.getMonth()];
+    var year = d.getFullYear();
+    const time=day + " " + hr + ":" + min + ampm + " " + date + " " + month + " " + year;
+    return time
   }
 }
 
