fd334ecce6c5bbf586e7246b8d32194d98390c14
NotTimIsReal <alastairrintumathew@gmail.com>
2021-09-23 12:30:47 +1000

    purges and a new better server-config command

---
+++ Constructors/serverconfig.js
@@ -0,0 +1,242 @@
+let cache=new Map()
+const { delay } = require('../handlers/functions')
+const {MessageActionRow, MessageSelectMenu, MessageButton}=require('discord.js')
+const chatbot = require('../Schemas/chatbot')
+const countSchema = require('../Schemas/countSchema')
+const logSchema=require('../Schemas/logSchema')
+const welcomeSchema = require('../Schemas/welcomeSchema')
+const serverConfSchema = require('../Schemas/serverConfSchema')
+const awaitWelcome=async(message)=>{
+    const f=i=>i.user.id===message.author.id&&i.componentType=='SELECT_MENU'
+    const filter=m=>m.author.id==message.author.id
+    let channels=[{label:'Disabled', description:'Use this to be disabled', value:'null'}, {label:'DM', description:'use this so that I will dm the user when they join', value:'null'}]
+                message.guild.channels.cache.forEach(channel=>{
+                    if(channel.type==='GUILD_TEXT'){
+                        channels.push({
+                            label:channel.name,
+                            description:channel.id.toString(),
+                            value:channel.id
+                        })
+                    }
+                })
+                const Enablerow = new MessageActionRow()
+                console.log(channels)
+                Enablerow
+                .addComponents(
+                    new MessageSelectMenu()
+                    .setCustomId('channels')
+                    .setPlaceholder('none')
+                    .addOptions(channels)
+                )
+                choose.reply({content:'Welcome Message', components: [Enablerow], ephemeral:false})
+                await message.channel.awaitMessageComponent(f)
+                .then(async(click)=>{
+                    if(click.values.toString()=='null'){
+                        click.deferReply()
+                        await welcomeSchema.deleteOne({_id:message.guild.id})
+                        click.reply('Disabled Welcome Message')
+                        return
+                    } 
+                    else if(click.values.toString()=='DM'){
+                        click.deferReply()
+                        click.followUp(`Tell me the message quick!`)
+                        click.deferUpdate()
+                        await message.channel.awaitMessages(filter)
+                        .then(async(msg)=>{
+                            msg=msg.first()
+                            await welcomeSchema.findOneAndUpdate({_id:message.guild.id},{_id:message.guild.id, channelID:null, DM:'true', text:msg.content}, {upsert:true})
+                        })
+                        click.followUp({content:'I suggest doing that command again but with the other sub-commands like member-log,etc as you may be missing out on a lot', ephemeral:true})
+                        return
+                    }else{
+                        click.deferReply()
+                        click.followUp(`Tell me the message quick!`)
+                        click.deferUpdate()
+                        await message.channel.awaitMessages(filter)
+                        .then(async(msg)=>{
+                            msg=msg.first()
+                            await welcomeSchema.findOneAndUpdate({_id:message.guild.id},{_id:message.guild.id, channelID:click.values.toString(), DM:'false', text:msg.content}, {upsert:true})
+                        })
+                        click.followUp({content:'I suggest doing that command again but with the other sub-commands like member-log,etc as you may be missing out on a lot', ephemeral:true})
+                        return 
+                    }
+                }).catch(err=>{console.log(err);return})
+                
+}
+const awaitChatbot=async(message)=>{
+    const f=i=>i.user.id===message.author.id&&i.componentType=='SELECT_MENU'
+    const filter=m=>m.author.id==message.author.id
+    let channels=[{label:'Disabled', description:'Use this to be disabled', value:'null'}]
+                message.guild.channels.cache.forEach(channel=>{
+                    if(channel.type==='GUILD_TEXT'){
+                        channels.push({
+                            label:channel.name,
+                            description:channel.id.toString(),
+                            value:channel.id
+                        })
+                    }
+                })
+                const Enablerow = new MessageActionRow()
+                console.log(channels)
+                Enablerow
+                .addComponents(
+                    new MessageSelectMenu()
+                    .setCustomId('channels')
+                    .setPlaceholder('null')
+                    .addOptions(channels)
+                )
+                choose.reply({content:'Chatbot', components: [Enablerow], ephemeral:false})
+                await message.channel.awaitMessageComponent(f)
+                .then(async(click)=>{
+                    if(click.values.toString()=='null'){
+                        click.deferReply()
+                        await chatbot.deleteOne({guildID:message.guild.id})
+                        click.reply('Disabled Chatbot')
+                        return
+                    } 
+                    else{
+                        click.deferReply()
+                        await chatbot.findOneAndUpdate({guildID:message.guild.id},{guildID:message.guild.id, channelID:click.values.toString()}, {upsert:true})
+                        click.followUp(`Added chatbot to <#${click.values.toString()}>`)
+                        click.deferUpdate()
+                        click.followUp({content:'I suggest doing that command again but with the other sub-commands like member-log,etc as you may be missing out on a lot', ephemeral:true})
+                        return
+                    }
+                })
+                
+}
+
+async function awaitMemberLog(message){
+    const f=i=>i.user.id===message.author.id&&i.componentType=='SELECT_MENU'
+    const filter=m=>m.author.id==message.author.id
+    let channels=[{label:'Disabled', description:'Use this to be disabled', value:'null'}]
+                message.guild.channels.cache.forEach(channel=>{
+                    if(channel.type==='GUILD_TEXT'){
+                        channels.push({
+                            label:channel.name,
+                            description:channel.id.toString(),
+                            value:channel.id
+                        })
+                    }
+                })
+                const Enablerow = new MessageActionRow()
+                console.log(channels)
+                Enablerow
+                .addComponents(
+                    new MessageSelectMenu()
+                    .setCustomId('channels')
+                    .setPlaceholder('null')
+                    .addOptions(channels)
+                )
+                choose.reply({content:'Member Log', components: [Enablerow], ephemeral:false})
+                await message.channel.awaitMessageComponent(f)
+                .then(async(click)=>{
+                    if(click.values.toString()=='null'){
+                        click.deferReply()
+                        await logSchema.deleteOne({guildID:message.guild.id})
+                        click.reply('Disabled MemberLog')
+                        return
+                    } 
+                    else{
+                        click.deferReply()
+                        await logSchema.findOneAndUpdate({guildID:message.guild.id},{guildID:message.guild.id, channelID:click.values.toString()}, {upsert:true})
+                        click.followUp(`Added Memberlog to <#${click.values.toString()}>`)
+                        click.deferUpdate()
+                        click.followUp({content:'I suggest doing that command again but with the other sub-commands like member-log,etc as you may be missing out on a lot', ephemeral:true})
+                        return
+                    }
+                })
+
+}
+async function awaitmemberCount(){
+    let channels=[{label:'Disabled', description:'Use this to be disabled', value:'null'}]
+                message.guild.channels.cache.forEach(channel=>{
+                    if(channel.type==='GUILD_VOICE'){
+                        channels.push({
+                            label:channel.name,
+                            description:channel.id.toString(),
+                            value:channel.id
+                        })
+                    }
+                })
+                const Enablerow = new MessageActionRow()
+                console.log(channels)
+                Enablerow
+                .addComponents(
+                    new MessageSelectMenu()
+                    .setCustomId('channels')
+                    .setPlaceholder('null')
+                    .addOptions(channels)
+                )
+                choose.reply({content:'Member-Count', components: [Enablerow], ephemeral:false})
+                await message.channel.awaitMessageComponent(f)
+                .then(async(click)=>{
+                    if(click.values.toString()=='null'){
+                        click.deferReply()
+                        await countSchema.deleteOne({guildID:message.guild.id})
+                        click.reply('Disabled membercount')
+                        return
+                    } 
+                    else{
+                        click.deferReply()
+                        await countSchema.findOneAndUpdate({guildID:message.guild.id},{guildID:message.guild.id, channelID:click.values.toString()}, {upsert:true})
+                        click.followUp(`Added membercount to <#${click.values.toString()}>`)
+                        click.deferUpdate()
+                        click.followUp({content:'I suggest doing that command again but with the other sub-commands like member-log,etc as you may be missing out on a lot', ephemeral:true})
+                        return
+                    }
+                })
+}
+async function awaitRoles(message){
+    const f=i=>i.user.id===message.author.id&&i.componentType=='SELECT_MENU'
+    const filter=m=>m.author.id==message.author.id
+    let roles=[]
+    message.guild.roles.cache.forEach(r=>{
+        roles.push({label:r.name, description:r.id.toString(), value:r.id.toString()})
+    })
+    const OwnerRoleId = new MessageActionRow()
+    console.log(roles)
+    const Enablerow=new MessageActionRow()
+    .addComponents(
+        new MessageSelectMenu()
+        .setCustomId('roles')
+        .setPlaceholder('null')
+        .addOptions(roles)
+    )
+    message.reply({content:'Owner! Choose Owner Role', components: [Enablerow], ephemeral:false})
+    let fullRoles=[]
+    await message.channel.awaitMessageComponent(f)
+    .then(async(click)=>{
+            click.deferReply()
+            fullRoles.push({owner:click.values.toString(), admin:null, member:null})
+            click.followUp(`Added <@!${click.values.toString()}> as a Owner Role`)
+    })
+    message.reply({content:'Admin! Choose Admin Role', components: [Enablerow], ephemeral:false})
+    await message.channel.awaitMessageComponent(f)
+    .then(async(click)=>{
+            click.deferReply()
+            fullRoles[0].admin=click.values.toString()
+            click.followUp(`Added <@!${click.values.toString()}> as a Admin Role`)
+    })
+    message.reply({content:'Member! Choose Member Role', components: [Enablerow], ephemeral:false})
+    await message.channel.awaitMessageComponent(f)
+    .then(async(click)=>{
+            click.deferReply()
+            fullRoles[0].member=click.values.toString()
+            click.followUp(`Added <@!${click.values.toString()}> as a Member Role`)
+    })
+    await serverConfSchema.findOneAndUpdate({_id:message.guild.id}, {
+        _id:message.guild.id,
+        memberroleID:roles[0].member,
+        adminroleID:roles[0].admin,
+        ownerroleID:roles[0].owner,
+    }, {upsert:true})
+}
+
+module.exports={
+    awaitWelcome,
+    awaitChatbot,
+    awaitMemberLog,
+    awaitmemberCount,
+    awaitRoles
+}
---
+++ commands/Administration/serverconfig.js
@@ -0,0 +1,92 @@
+const { awaitChatbot, awaitWelcome, awaitmemberCount, awaitMemberLog, awaitRoles } = require("../../Constructors/serverconfig")
+const {MessageActionRow, MessageSelectMenu}=require('discord.js')
+module.exports = {
+    name: "serverconfig",
+    aliases: ["conf"],
+    description: "Set-up the server",
+    category: "Administration",
+    memberpermissions:"ADMINISTRATOR",
+    cooldown: 10,
+    usage: "serverconf",
+    run:async(client, message, args)=>{
+        const f=i=>i.user.id===message.author.id&&i.componentType=='SELECT_MENU'
+        const filter=m=>m.author.id==message.author.id
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageSelectMenu()
+                .setCustomId('select')
+                .setPlaceholder('Nothing selected')
+                .addOptions([
+                    {
+                        label: 'Chatbot',
+                        description: 'Chatbot',
+                        value: 'chatbot',
+                    },
+                    {
+                        label: 'membercount',
+                        description: 'Member count',
+                        value: 'mc',
+                    },
+                    {
+                        label: 'memberlog',
+                        description: 'Member Log',
+                        value: 'ml',
+                    },
+                    {
+                        label: 'welcomeMessage',
+                        description: 'Welcome Message',
+                        value: 'wm',
+                    },
+                    {
+                        label: 'roles',
+                        description: 'Roles',
+                        value: 'roles',
+                    },
+                ]),
+        );
+
+        message.reply({ content: 'Choose your setting', components: [row] });
+        await message.channel.awaitMessageComponent(f)
+        .then(async(choose)=>{
+            if(choose.values.toString()=='chatbot'){
+                await awaitChatbot(message)
+                return message.channel.send('Config Done')
+            }
+            else if(choose.values.toString()=='wm'){
+                await awaitWelcome(message)
+                return message.channel.send('Config Done')
+            }
+            else if(choose.values.toString()=='mc'){
+                await awaitmemberCount(message)
+                return message.channel.send('Config Done')
+            }
+            else if(choose.values.toString()=='ml'){
+                await awaitMemberLog(message)
+                return message.channel.send('Config Done') 
+            }
+            else if(choose.values.toString()=='roles'){
+                await awaitRoles(message)
+                return message.channel.send('Config Done')
+            }
+
+
+        }).catch(err=>{
+            return console.log(err)
+        })
+        return 
+        //code
+        /*
+        const req=require('../../serverconfig')
+        let collector;
+        await req.chatBot(message, co, state)
+        delay(100000)
+        await req.memberCount(message, co, state)
+        delay(100000)
+        await req.memberLog(message, co, state)
+        delay(10000)
+        await req.welcomeMes(message, co, state)
+        delay(10000)
+        await req.roles(message, co, state)
+        message.channel.send('Server Config Completed')
+        console.log(cache)*/
+}}
