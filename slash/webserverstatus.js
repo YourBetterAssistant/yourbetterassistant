0cad015191dc176920bc5935dc4a611222f124d9
NotTimIsReal <alastairrintumathew@gmail.com>
2021-10-11 20:26:29 +1100

    Added WebServerStatus

---
+++ slash/webserverstatus.js
@@ -0,0 +1,22 @@
+var https = require("https");
+module.exports = {
+    name: "webserverstatus",
+    description: "Shows Status of https://yourbetterassistant.me",
+    guild:true,
+    run:async(client, interaction)=>{
+        try{
+            https.get({host: "yourbetterassistant.me"}, function(res){
+                if( res.statusCode === 200 )
+                    return interaction.reply('The Webserver Returned A 200 OK!')
+                else
+                        interaction.reply('The Webserver Returned a '+res.statusCode+' '+res.statusMessage)
+                        interaction.followUp("I don't think that is good I will send a bug report on behalf of you")
+                        return client.channels.cache.get('897048482366521436').send(`<@!827388013062389761> https://yourbetterassistant.me returned a ${res.statusCode} ${res.statusMessage}`)
+                });
+        }catch(err){
+            interaction.reply('An Error Occured While Pinging The Webserver A bug report will be sent')
+            return client.channels.cache.get('897048482366521436').send(`<@!827388013062389761> https://yourbetterassistant.me returned an unknown error this error occured while running \`webserverstatus.js\``)
+
+        }
+
+    }}
