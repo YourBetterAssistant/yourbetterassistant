4ce07c2a5a15a213421d893057b8d0e991500c75
NotTimIsReal <alastairrintumathew@gmail.com>
2021-10-14 09:08:06 +1100

    new delete commands

--- slash/deleteAllCommands.js
+++ slash/deleteAllCommands.js
@@ -1,11 +1,19 @@
 module.exports = {
     name: "deletecommands",
     description: "delete",
-    options:[],
+    options:[{type:5, name:'guildsonly', description:'Guild Only', required:true }],
+    guild:true,
     run:async(client, interaction)=>{
+        const guild=interaction.options?.getBoolean('guildsonly')
         if(interaction.user.id !== '827388013062389761')return interaction.reply({content:'You Are Not Allowed to Execute This Command', ephemeral:true})
-        interaction.deferReply()
-        await client.application.commands.fetch().then((command)=>command.delete())
-        await client.guilds.cache.get(interaction.guild.id)?.commands.fetch().then((command)=>command.delete())
-        interaction.editReply('Done')
+        interaction.reply('Contacting API...')
+
+        if(guild===true){
+            await client.guilds.cache.get(interaction.guild.id)?.commands.fetch().then((command)=>command.delete())
+        }
+        else{
+            await client.application.commands.fetch().then((command)=>command.delete())
+            await client.guilds.cache.get(interaction.guild.id)?.commands.fetch().then((command)=>command.delete())
+        }  
+        interaction.followUp('Done')
     }}
