0288b6dc8aa1bc52a0d86c79e69dedab69adf51a
NotTimIsReal <alastairrintumathew@gmail.com>
2021-10-19 19:15:42 +1100

    time fluctuates

--- slash/webserverstatus.js
+++ slash/webserverstatus.js
@@ -4,16 +4,17 @@ module.exports = {
     description: "Shows Status of https://yourbetterassistant.me",
     run:async(client, interaction)=>{
         try{
+            interaction.deferReply()
             https.get({host: "yourbetterassistant.me"}, function(res){
                 if( res.statusCode === 200 )
-                    return interaction.reply('The Webserver Returned A 200 OK!')
+                    return interaction.editReply('The Webserver Returned A 200 OK!')
                 else
-                        interaction.reply('The Webserver Returned a '+res.statusCode+' '+res.statusMessage)
+                        interaction.editReply('The Webserver Returned a '+res.statusCode+' '+res.statusMessage)
                         interaction.followUp("I don't think that is good I will send a bug report on behalf of you")
                         return client.channels.cache.get('897048482366521436').send(`<@!827388013062389761> https://yourbetterassistant.me returned a ${res.statusCode} ${res.statusMessage}`)
                 });
         }catch(err){
-            interaction.reply('An Error Occured While Pinging The Webserver A bug report will be sent')
+            interaction.editReply('An Error Occured While Pinging The Webserver A bug report will be sent')
             return client.channels.cache.get('897048482366521436').send(`<@!827388013062389761> https://yourbetterassistant.me returned an unknown error this error occured while running \`webserverstatus.js\``)
 
         }
