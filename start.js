6b79cb662fe2e159d61685c1a367d45508af96bd
NotTimIsReal <alastairrintumathew@gmail.com>
2021-10-20 17:20:07 +1100

    changed my mind

--- start.js
+++ start.js
@@ -3,17 +3,12 @@ const axios=require('axios').default
 const url=[]
 const shell=require('shelljs')
 const fs=require('fs')
-fs.unlink('err-0.log', err=>console.log(err))
-fs.unlink('logs-0.log', err=>console)
 url.push(process.env.URL)
 async function push(){
     if(process.env.URL){
         await axios.post(url[0], {content:">>> Uptime For YourBetterAssistant"}, {headers:{'Content-Type':'application/json'}})
     }
 }
-push().then(()=>{
-    shell.exec('pm2 start ecosystem.config.js&')
-})
-
-process.exit(1)
+push()
+require('./index')
 
