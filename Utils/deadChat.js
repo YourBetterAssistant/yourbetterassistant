9067676abc637fc91856b689904bb5daf3d23eb4
NotTimIsReal <alastairrintumathew@gmail.com>
2021-10-28 19:48:09 +1100

    hopefully this works

---
+++ Utils/deadChat.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+const reddit=require('better-redddit')
+const ch=[]
+module.exports = async function deadChat(client,){
+    ch.length=0
+    for(let guild of client.guilds.cache){
+        guild=guild[1]
+        for(const channels of (guild.channels.cache).filter(c=>c.type =='GUILD_TEXT'&&c.name !='member-log')){
+            ch.push({id:guild.id, channels})         
+        }
+       const channel=ch.filter(c=>c.id===guild.id)[Math.floor(Math.random()*ch.length)].channels[1]
+       if(channel){
+        let lastMessage=(await channel.messages.fetch({limit: 1})).first()
+            //1,200,000
+            if(lastMessage){
+                const timestamp=Date.now()-lastMessage.createdTimestamp
+                if(timestamp > 0){
+                    if(timestamp >= 1200000){
+                        if(lastMessage.channel){
+                            const post=(await reddit.top_posts("wholesome", 10))[Math.floor(Math.random() * 10)].data
+                            channel.send('Hey People Wake Up!, feel bored? Here is something wholesome from reddit '+ post.url)
+            
+                        
+                        }
+                    }
+        
+                }
+
+            }}
+}
+
+}
