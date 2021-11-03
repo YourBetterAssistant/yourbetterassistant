5ec9045956c91b9fc003bdb1f164b6f5a9310fc9
NotTimIsReal <alastairrintumathew@gmail.com>
2021-11-03 21:49:33 +1100

    made mute into slash commands

---
+++ slash/mute.js
@@ -0,0 +1,126 @@
+const { MessageEmbed } = require("discord.js");
+const serverConfSchema = require("../Schemas/serverConfSchema");
+const roles = {};
+module.exports = {
+  name: "mute",
+  description: "Mute",
+  guild: true,
+  options: [
+    { name: "user", description: "user", type: 6, required: true },
+    {
+      name: "duration",
+      description: "duration",
+      type: 10,
+      required: true,
+      choices: [
+        { name: "1 Day", value: 86400000 },
+        { name: "10 hours", value: 36000000 },
+        { name: "5 hours", value: 18000000 },
+        { name: "1 Hour", value: 3600000 },
+        { name: "10 minutes", value: 600000 },
+        { name: "1 minute", value: 60000 },
+      ],
+    },
+    { name: "reason", description: "reason", type: 3 },
+  ],
+  run: async (client, interaction) => {
+    const user = interaction.options.getMember("user");
+    const reason = interaction.options.getString("reason");
+    const duration = interaction.options.getNumber("duration");
+    if (interaction.guild.roles.everyone.permissions.has("SEND_MESSAGES"))
+      return interaction.reply({
+        content:
+          "The `@everyone` role has the permissions `SEND_MESSAGES` leaving this on will make muting users useless, TO fix this error un-toggle send messages for `@everyone`",
+        ephemeral: true,
+      });
+    let result = await serverConfSchema.findOne({
+      _id: interaction.guild.id,
+    });
+    let admin = result.adminroleID;
+    let member = result.memberroleID;
+    let owner = result.ownerroleID;
+    roles[interaction.guild.id] = { admin, member, owner };
+    let memberrole = roles[interaction.guild.id].member;
+    if (!memberrole)
+      return interaction.reply({
+        content:
+          "I cannot mute without a member role, please do b!serverconfig to set up `ROLES` so that i can give the user the specified roles for member when their mute is up",
+        ephemeral: true,
+      });
+    if (!interaction.member.permissions.has("MANAGE_ROLES"))
+      return interaction.reply({
+        content: "Invalid Permissions, Expected Perms `MANAGE_ROLES`",
+        ephemeral: true,
+      });
+    if (user.permissions.has("MANAGE_GUILD"))
+      return interaction.reply({
+        content:
+          "The user who you attempted to mute has the permissions `MANAGE_GUILD` I am not allowed to mute people with such permissions",
+        ephemeral: true,
+      });
+    let muterole = interaction.guild.roles.cache.find(
+      (role) => role.name === "muted"
+    );
+    if (!muterole) {
+      interaction.guild.roles
+        .create({
+          data: {
+            name: "muted",
+            color: "BLUE",
+            permissions: ["VIEW_CHANNEL"],
+          },
+          reason: "muted role does not exist",
+        })
+        .catch(console.error);
+    }
+    user.roles.set([muterole]);
+    const stringedDuration = duration.toString();
+    const embed = new MessageEmbed()
+      .setTitle("Mute")
+      .setDescription(`${user} was muted by ${interaction.member}`)
+      .addField("Reason:", reason, true)
+      .addField(
+        "Duration:",
+        stringedDuration == "86400000"
+          ? "1 Day"
+          : stringedDuration == "36000000"
+          ? "10 Hours"
+          : stringedDuration === "18000000"
+          ? "5 Hours"
+          : stringedDuration === "3600000"
+          ? "1 Hour"
+          : stringedDuration === "600000"
+          ? "10 Minutes"
+          : stringedDuration === "60000"
+          ? "1 Minute"
+          : "Infinity(ERR_UNKNOWN)",
+        true
+      )
+      .setColor("RANDOM");
+    interaction.reply({ embeds: [embed] });
+    setTimeout(function () {
+      user.roles.set([memberrole]);
+      let embed = new MessageEmbed()
+        .setTitle("Unmute")
+        .setDescription(
+          `${user}'s mute has ended. The mute lasted ${
+            stringedDuration == "86400000"
+              ? "1 Day"
+              : stringedDuration == "36000000"
+              ? "10 Hours"
+              : stringedDuration === "18000000"
+              ? "5 Hours"
+              : stringedDuration === "3600000"
+              ? "1 Hour"
+              : stringedDuration === "600000"
+              ? "10 Minutes"
+              : stringedDuration === "60000"
+              ? "1 Minute"
+              : "Infinity(ERR_UNKNOWN)"
+          }`
+        )
+        .setColor("RANDOM");
+      interaction.channel.send({ embeds: [embed] });
+    }, duration);
+  },
+};
---
+++ slash/unmute.js
@@ -0,0 +1,43 @@
+const { MessageEmbed } = require("discord.js");
+const serverConfSchema = require("../Schemas/serverConfSchema");
+const roles = {};
+module.exports = {
+  name: "unmute",
+  description: "unmute",
+  options: [{ name: "user", description: "user", type: 6 }],
+  run: async (client, interaction) => {
+    let user = interaction.options.getUser("user");
+    let result = await serverConfSchema.findOne({
+      _id: interaction.guild.id,
+    });
+    let admin = result.adminroleID;
+    let member = result.memberroleID;
+    let owner = result.ownerroleID;
+    roles[interaction.guild.id] = { admin, member, owner };
+    let memberrole = roles[interaction.guild.id].member;
+    if (!memberrole)
+      return interaction.reply({
+        content:
+          "I cannot unmute without a member role, please do b!serverconfig to set up `ROLES` so that i can give the user the specified roles for member when their unmute is up",
+        ephemeral: true,
+      });
+    if (!interaction.member.permissions.has("MANAGE_ROLES"))
+      return interaction.reply({
+        content: "Invalid Permissions, Expected Perms `MANAGE_ROLES`",
+        ephemeral: true,
+      });
+    if (user.permissions.has("MANAGE_GUILD"))
+      return interaction.reply({
+        content:
+          "The user who you attempted to unmute has the permissions `MANAGE_GUILD` I am not allowed to unmute people with such permissions",
+        ephemeral: true,
+      });
+    user.roles.set([memberrole]);
+    let embed = new MessageEmbed()
+      .setTitle("Unmute")
+      .setDescription(`${user}'s mute has ended`)
+      .setColor("RANDOM");
+    interaction.channel.send({ embeds: [embed] });
+  },
+};
+//https://api.weky.xyz/canvas/whodidthis?image=${img}
