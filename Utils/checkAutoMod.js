82803ba657c9fa8240b1c93d4e86305e821ea703
NotTimIsReal <alastairrintumathew@gmail.com>
2021-11-07 19:33:39 +1100

    fixed replying bug

--- Utils/checkAutoMod.js
+++ Utils/checkAutoMod.js
@@ -1,32 +1,34 @@
 const autoMod = require("../Schemas/autoMod");
-const autoModCache=[]
-async function checkAutoMod(message){
-    console.log('running')
-    let result=await autoModCache.find(i=>i.id==message.guild.id)
-    if(!result){
-        let result=await autoMod.findOne({guildId:message.guild.id})
-        console.log('new cache for automod')
-        if(!result){
-            autoModCache.push({id:message.guild.id, strictmode:'none'})
-        }else{
-            autoModCache.push({id:message.guild.id, strictmode:result.strictMode.toString()})
-        }
-    }else{
-        console.log('cache')
+const autoModCache = [];
+async function checkAutoMod(message) {
+  console.log("running");
+  let result = await autoModCache.find((i) => i.id == message.guild.id);
+  if (!result) {
+    let result = await autoMod.findOne({ guildId: message.guild.id });
+    console.log("new cache for automod");
+    if (!result) {
+      autoModCache.push({ id: message.guild.id, strictmode: "none" });
+    } else {
+      autoModCache.push({
+        id: message.guild.id,
+        strictmode: result.strictMode.toString(),
+      });
     }
-    result=await autoModCache.find(i=>i.id==message.guild.id)
-    return result
-    
+  } else {
+    console.log("cache");
+  }
+  result = await autoModCache.find((i) => i.id == message.guild.id);
+  console.log(result);
+  return result;
 }
-async function forceNewCache(){
-        autoModCache.length=0
-        // let result=await autoMod.findOne({guildId:message.guild.id})
-        // if(!result.guildId){
-        //     autoModCache.push({id:message.guild.id, strictmode:'none'})
-        // }else{
-        //     autoModCache.push({id:message.guild.id, strictmode:result.strictMode})
-        // }
-    
+async function forceNewCache() {
+  autoModCache.length = 0;
+  // let result=await autoMod.findOne({guildId:message.guild.id})
+  // if(!result.guildId){
+  //     autoModCache.push({id:message.guild.id, strictmode:'none'})
+  // }else{
+  //     autoModCache.push({id:message.guild.id, strictmode:result.strictMode})
+  // }
 }
-exports.checkAutoMod=checkAutoMod
-exports.forceAutoCacheMod=forceNewCache
+exports.checkAutoMod = checkAutoMod;
+exports.forceAutoCacheMod = forceNewCache;
--- commands/Games/chess.js
+++ commands/Games/chess.js
@@ -1,20 +1,26 @@
-'use strict';
+"use strict";
 
-const {DiscordTogether}=require('discord-together')
-const {reply}=require('../../exports')
+const { DiscordTogether } = require("discord-together");
 module.exports = {
-    name: "chess",
-    description: "Play chess with your buds",
-    category: "Games",
-    guildOnly: true,
-    memberpermissions:"VIEW_CHANNEL",
-    cooldown: 10,
-    usage: "chess",
-    run:async(client, message)=>{
-        client.discordTogether = new DiscordTogether(client);
-        if(!message.member.voice.channel)return reply('You need to be in a VC')
-        client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'chess').then(async invite => {
-            if(invite.code =='https://discord.com/invite/50013')return message.channel.send('An error occured while creating that invite, make sure I have the permission: `CREATE-INVITE` and that this channel allows me to send invites')
-            else return message.channel.send(`${invite.code}`);
-    })
-}}
+  name: "chess",
+  description: "Play chess with your buds",
+  category: "Games",
+  guildOnly: true,
+  memberpermissions: "VIEW_CHANNEL",
+  cooldown: 10,
+  usage: "chess",
+  run: async (client, message) => {
+    client.discordTogether = new DiscordTogether(client);
+    if (!message.member.voice.channel)
+      return message.reply("You need to be in a VC");
+    client.discordTogether
+      .createTogetherCode(message.member.voice.channel.id, "chess")
+      .then(async (invite) => {
+        if (invite.code == "https://discord.com/invite/50013")
+          return message.channel.send(
+            "An error occured while creating that invite, make sure I have the permission: `CREATE-INVITE` and that this channel allows me to send invites"
+          );
+        else return message.channel.send(`${invite.code}`);
+      });
+  },
+};
