0da759fa50c4c50976a2401b2cfaff6a36a1828b
NotTimIsReal <alastairrintumathew@gmail.com>
2021-11-07 20:15:02 +1100

    removed help.js and made it a slash command

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -1,43 +1,58 @@
-'use strict';
-
-const fs=require('fs')
-const rrSchema = require("../../Schemas/rrSchema")
-const Discord=require('discord.js')
-module.exports=async(client, interaction)=>{
-    if(interaction.isCommand()){    
-        let cmd=client.interactions.get(interaction.commandName)
-        if(!cmd)return
-        cmd.run(client, interaction, Discord)
-    }
-    else if(interaction.isButton()){
-        if(interaction.guild){
-            const possibleReaction=await rrSchema.findOne({guildId:interaction.guild.id})
-            if(possibleReaction&&interaction.message.id===possibleReaction.messageId){
-                if(interaction.customId==possibleReaction.role1.name){
-                    interaction.guild.roles.fetch()
-                    const role=interaction.guild.roles.cache.get(possibleReaction.role1.id)  
-                    if(interaction.member.roles.cache.get(possibleReaction.role1.id)){
-                        interaction.member.roles.remove(role)
-                        interaction.reply({content:`The role ${role} has been removed from you click the button again to get it back`, ephemeral:true})
-                    }
-                    else{
-                        interaction.member.roles.add(role)
-                        interaction.reply({content:`The role ${role} has been added to you click the button again to lose it`, ephemeral:true})
-                    }
-                }else if(interaction.customId===possibleReaction.role2.name){
-                    interaction.guild.roles.fetch()
-                    const role=interaction.guild.roles.cache.get(possibleReaction.role2.id)  
-                    if(interaction.member.roles.cache.get(possibleReaction.role2.id)){
-                        interaction.member.roles.remove(role)
-                        interaction.reply({content:`The role ${role} has been removed from you click the button again to get it back`, ephemeral:true})
-                        
-                    }
-                    else{
-                        interaction.member.roles.add(role)
-                        interaction.reply({content:`The role ${role} has been added to you click the button again to lose it`, ephemeral:true})
-                    }
-                }
-            }
+"use strict";
+const rrSchema = require("../../Schemas/rrSchema");
+const Discord = require("discord.js");
+module.exports = async (client, interaction) => {
+  if (interaction.isCommand()) {
+    let cmd = client.interactions.get(interaction.commandName);
+    if (!cmd) return;
+    cmd.run(client, interaction, Discord);
+  } else if (interaction.isButton()) {
+    if (interaction.guild) {
+      const possibleReaction = await rrSchema.findOne({
+        guildId: interaction.guild.id,
+      });
+      if (
+        possibleReaction &&
+        interaction.message.id === possibleReaction.messageId
+      ) {
+        if (interaction.customId == possibleReaction.role1.name) {
+          interaction.guild.roles.fetch();
+          const role = interaction.guild.roles.cache.get(
+            possibleReaction.role1.id
+          );
+          if (interaction.member.roles.cache.get(possibleReaction.role1.id)) {
+            interaction.member.roles.remove(role);
+            interaction.reply({
+              content: `The role ${role} has been removed from you click the button again to get it back`,
+              ephemeral: true,
+            });
+          } else {
+            interaction.member.roles.add(role);
+            interaction.reply({
+              content: `The role ${role} has been added to you click the button again to lose it`,
+              ephemeral: true,
+            });
+          }
+        } else if (interaction.customId === possibleReaction.role2.name) {
+          interaction.guild.roles.fetch();
+          const role = interaction.guild.roles.cache.get(
+            possibleReaction.role2.id
+          );
+          if (interaction.member.roles.cache.get(possibleReaction.role2.id)) {
+            interaction.member.roles.remove(role);
+            interaction.reply({
+              content: `The role ${role} has been removed from you click the button again to get it back`,
+              ephemeral: true,
+            });
+          } else {
+            interaction.member.roles.add(role);
+            interaction.reply({
+              content: `The role ${role} has been added to you click the button again to lose it`,
+              ephemeral: true,
+            });
+          }
         }
+      }
     }
-}
+  }
+};
---
+++ slash/help.js
@@ -0,0 +1,119 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../botconfig/config.json");
+const ee = require("../botconfig/embed.json");
+module.exports = {
+  name: "help",
+  description: "Shows All Message Commands",
+  options: [
+    {
+      type: 3,
+      name: "command",
+      description: "Shows Information Of Specific Command",
+    },
+  ],
+  run: async (client, interaction) => {
+    const command = interaction.options.getString("command");
+    try {
+      if (command) {
+        const embed = new MessageEmbed();
+        const cmd =
+          client.commands.get(command.toLowerCase()) ||
+          client.commands.get(client.aliases.get(command.toLowerCase()));
+        if (!cmd) {
+          embed
+            .setColor(ee.wrongcolor)
+            .setDescription(
+              `No Information found for command **${command.toLowerCase()}**`
+            );
+          return interaction.reply({ embeds: [embed] });
+        }
+        if (cmd.name) embed.addField("**Command :**", `\`${cmd.name}\``);
+        if (cmd.name)
+          embed.setTitle(`Detailed Information about:\`${cmd.name}\``);
+        if (cmd.description)
+          embed.addField("**Description**", `\`${cmd.description}\``);
+        if (cmd.aliases)
+          embed.addField(
+            "**Aliases**",
+            `\`${cmd.aliases.map((a) => `${a}`).join("`, `")}\``
+          );
+        if (cmd.cooldown)
+          embed.addField("**Cooldown**", `\`${cmd.cooldown} Seconds\``);
+        else
+          embed.addField(
+            "**Cooldown**",
+            `\`${config.defaultCommandCooldown}\``
+          );
+        if (cmd.usage) {
+          embed.addField("**Usage**", `\`${config.prefix}${cmd.usage}\``);
+          embed.setFooter("Syntax: <> = required, [] = optional");
+        }
+        if (cmd.useage) {
+          embed.addField("**Usage**", `\`${config.prefix}${cmd.useage}\``);
+          embed.addField(
+            "Your prefix could have been changed, ping the bot to double check!"
+          );
+          embed.setFooter("Syntax: <> = required, [] = optional");
+        }
+        embed.setColor(ee.color);
+        return interaction.reply({ embeds: [embed] });
+      } else {
+        const embed = new MessageEmbed()
+          .setColor(ee.color)
+          .setThumbnail(client.user.displayAvatarURL())
+          .setTitle("HELP MENU 🔰 Commands")
+          .setFooter(
+            `To see command descriptions and inforamtion, type: ${config.prefix}help [CMD NAME]`,
+            client.user.displayAvatarURL()
+          );
+        const commands = (category) => {
+          return client.commands
+            .filter((cmd) => cmd.category === category)
+            .map((cmd) => `\`${cmd.name}\``);
+        };
+        try {
+          for (let i = 0; i < client.categories.length; i += 1) {
+            const current = client.categories[i];
+            const items = commands(current);
+            const n = 3;
+            const result = [[], [], []];
+            const wordsPerLine = Math.ceil(items.length / 3);
+            for (let line = 0; line < n; line++) {
+              for (let i = 0; i < wordsPerLine; i++) {
+                const value = items[i + line * wordsPerLine];
+                if (!value) continue;
+                result[line].push(value);
+              }
+            }
+            embed.addField(
+              `**${current.toUpperCase()} [${items.length}]**`,
+              `> ${result[0].join("\n> ")}`,
+              true
+            );
+            embed.addField(
+              `\u200b`,
+              `${result[1].join("\n") ? result[1].join("\n") : "\u200b"}`,
+              true
+            );
+            embed.addField(
+              `\u200b`,
+              `${result[2].join("\n") ? result[2].join("\n") : "\u200b"}`,
+              true
+            );
+          }
+        } catch (e) {
+          console.log(String(e.stack).red);
+        }
+        interaction.reply({ embeds: [embed] });
+      }
+    } catch (e) {
+      console.log(String(e.stack).bgRed);
+      let embed = new MessageEmbed()
+        .setColor(ee.wrongcolor)
+        .setFooter(ee.footertext, ee.footericon)
+        .setTitle(`❌ ERROR | An error occurred`)
+        .setDescription(`\`\`\`${e.stack}\`\`\``);
+      return interaction.reply({ embeds: [embed], ephemeral: true });
+    }
+  },
+};
