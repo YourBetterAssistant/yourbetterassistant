ec4dd0d758dc2d49dd7166006c9276ba33e810a9
NotTimIsReal <alastairrintumathew@gmail.com>
2021-11-14 18:22:22 +1100

    Added computer eval

---
+++ commands/OWNER/computer-eval.js
@@ -0,0 +1,24 @@
+const { exec } = require("child_process");
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "sys-eval",
+  description: "OWNER",
+  category: "OWNER",
+  memberpermissions: ["VIEW_CHANNEL"],
+  cooldown: 20,
+  usage: "systemeval <eval>",
+  run: async (client, message, args) => {
+    if (message.author.id !== "827388013062389761")
+      return message.channel.send("You are not the owner of this bot!");
+    else
+      exec(args.join(" "), (err, stdout, stderr) => {
+        const embed = new MessageEmbed()
+          .setTitle(`You Executed\`\`\`${args.join(" ")}\`\`\``)
+          .setColor("RED");
+        if (err) embed.addField("Error", `\`\`\`sh\n${err.stack}\n\`\`\``);
+        if (stdout) embed.addField("Out", `\`\`\`sh\n${stdout}\n\`\`\``);
+        if (stderr) embed.addField("stderr", `\`\`\`sh\n${stderr}\n\`\`\``);
+        message.channel.send({ embeds: [embed] });
+      });
+  },
+};
