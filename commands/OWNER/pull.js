f7225b03c9d5fc53bfa0093df0f4312ffe6ebcbd
NotTimIsReal <alastairrintumathew@gmail.com>
2022-01-11 18:46:59 +1100

    readded lock files and formatted pull.js

--- commands/OWNER/pull.js
+++ commands/OWNER/pull.js
@@ -1,42 +1,44 @@
-'use strict';
+"use strict";
 
-const shell=require('shelljs')
-const {reply}=require('../../exports')
+const { exec } = require("child_process");
+const { reply } = require("../../exports");
 module.exports = {
-    name: "pull",
-    description: "pulls the latest version of this code from github",
-    category: "OWNER",
-    memberpermissions:"VIEW_CHANNEL",
-    cooldown: 2,
-    usage: "pull",
-    run:async(client, message, args)=>{
-        let ownerId="827388013062389761"
-        if(message.author.id!==ownerId){
-            message.channel.send('You cannot pull');
-            console.log('bruh')
-            return
+  name: "pull",
+  description: "pulls the latest version of this code from github",
+  category: "OWNER",
+  memberpermissions: "VIEW_CHANNEL",
+  cooldown: 2,
+  usage: "pull",
+  run: async (client, message) => {
+    let ownerId = "827388013062389761";
+    if (message.author.id !== ownerId) {
+      message.channel.send("You cannot pull");
+      console.log("bruh");
+      return;
+    }
+
+    let link = "https://www.github.com/NotTimIsReal/bbarevamp";
+    reply(`Pulling from **${link}**`, true, message);
+    exec(`git pull ${link}`, (err) => {
+      if (err) return message.reply(`Error: \`\`\`${err.stack}\`\`\``);
+    });
+
+    message.reply("Do you want me to reboot");
+    let filter = (m) => m.author.id === message.author.id;
+    message.channel
+      .awaitMessages({
+        filter,
+        max: 1,
+        time: 10000,
+        errors: ["time"],
+      })
+      .then((msg) => {
+        msg = msg.first();
+        if (msg.content.startsWith("yes")) {
+          exec("pm2 restart 0");
+        } else {
+          return message.channel.send("Cancelled");
         }
-          
-        let link='https://www.github.com/NotTimIsReal/bbarevamp'
-        reply(`Pulling from **${link}**`, true, message)
-        shell.exec(`git pull ${link}`)
-        
-        message.reply('Do you want me to reboot')
-        let filter=m=>m.author.id===message.author.id
-        message.channel.awaitMessages({
-            filter, 
-            max:1,
-            time:10000,
-            errors:['time'],
-        }).then(msg=>{
-            msg=msg.first()
-            if(msg.content.startsWith('yes')){
-                shell.exec('pm2 restart 0')}else{
-                    return message.channel.send('Cancelled')
-                }
-
-        })
-
-        
-    },
+      });
+  },
 };
