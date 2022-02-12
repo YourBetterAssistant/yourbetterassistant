2ab9026f8a51222864dcc0ecb0853845108aa05b
NotTimIsReal <alastairrintumathew@gmail.com>
2022-02-12 15:36:37 +1100

    Games comamnd updated

--- commands/Games/chess.ts
+++ commands/Games/chess.ts
@@ -1,7 +1,8 @@
 "use strict";
 
-const { DiscordTogether } = require("discord-together");
-module.exports = {
+import { DiscordTogether } from "discord-together";
+import { Client, Message } from "discord.js";
+export default {
   name: "chess",
   description: "Play chess with your buds",
   category: "Games",
@@ -9,13 +10,13 @@ module.exports = {
   memberpermissions: "VIEW_CHANNEL",
   cooldown: 10,
   usage: "chess",
-  run: async (client, message) => {
+  run: async (client: any, message: Message) => {
     client.discordTogether = new DiscordTogether(client);
-    if (!message.member.voice.channel)
+    if (!message.member?.voice.channel)
       return message.reply("You need to be in a VC");
     client.discordTogether
       .createTogetherCode(message.member.voice.channel.id, "chess")
-      .then(async (invite) => {
+      .then(async (invite: { code: string }) => {
         if (invite.code == "https://discord.com/invite/50013")
           return message.channel.send(
             "An error occured while creating that invite, make sure I have the permission: `CREATE-INVITE` and that this channel allows me to send invites"
--- commands/Games/youtube.ts
+++ commands/Games/youtube.ts
@@ -1,20 +1,28 @@
-'use strict';
+"use strict";
 
-const {DiscordTogether}=require('discord-together')
-const {reply}=require('../../exports')
-module.exports = {
-    name: "youtube",
-    description: "Play youtube with your buds",
-    category: "Games",
-    guildOnly: true,
-    memberpermissions:"VIEW_CHANNEL",
-    cooldown: 10,
-    usage: "youtube",
-    run:async(client, message)=>{
-        client.discordTogether = new DiscordTogether(client);
-        if(!message.member.voice.channel)return reply('You need to be in a VC', true, message)
-        client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
-            if(invite.code =='https://discord.com/invite/50013')return message.channel.send('An error occured while creating that invite, make sure I have the permission: `CREATE-INVITE` and that this channel allows me to send invites')
-            else return message.channel.send(`${invite.code}`);
-    })
-}}
+import { DiscordTogether } from "discord-together";
+import { Client, Message } from "discord.js";
+import { reply } from "../../exports";
+export default {
+  name: "youtube",
+  description: "Play youtube with your buds",
+  category: "Games",
+  guildOnly: true,
+  memberpermissions: "VIEW_CHANNEL",
+  cooldown: 10,
+  usage: "youtube",
+  run: async (client: any, message: Message) => {
+    const discordTogether = new DiscordTogether(client);
+    if (!message.member?.voice.channel)
+      return reply("You need to be in a VC", true, message);
+    discordTogether
+      .createTogetherCode(message.member.voice.channel.id, "youtube")
+      .then(async (invite) => {
+        if (invite.code == "https://discord.com/invite/50013")
+          return message.channel.send(
+            "An error occured while creating that invite, make sure I have the permission: `CREATE-INVITE` and that this channel allows me to send invites"
+          );
+        else return message.channel.send(`${invite.code}`);
+      });
+  },
+};
