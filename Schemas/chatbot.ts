a02199f64e239f1aa0372dfa8f23df7d20c99776
NotTimIsReal <alastairrintumathew@gmail.com>
2022-02-20 18:32:29 +1100

    Fixed Spelling for Chatbot

--- Schemas/chatbot.ts
+++ Schemas/chatbot.ts
@@ -1,16 +1,19 @@
-'use strict';
+"use strict";
 
-import mongoose from 'mongoose';
-const chatBot=new mongoose.Schema({
-    guildID: {
-        type: String,
-        required: true
-    },
-    channelID: {
-        type: String,
-        required: true
-    }
-}) 
+import mongoose from "mongoose";
+const chatBot = new mongoose.Schema({
+  guildID: {
+    type: String,
+    required: true,
+  },
+  channelID: {
+    type: String,
+    required: true,
+  },
+});
 
-
-export default mongoose.model('chatBot', chatBot)
+export default mongoose.model<Ichatbot>("chatbots", chatBot);
+interface Ichatbot {
+  guildID: string;
+  channelID: string;
+}
--- Utils/checkChatChannel.ts
+++ Utils/checkChatChannel.ts
@@ -1,29 +1,29 @@
-import axios from "axios"
-import chatBot from '../Schemas/chatbot'
-async function check(message){
-    let check=await chatBot.findOne({guildID:message.guild.id})
-    if(check){
-      let ch=check.channelID
-      if(message.channel.id===ch){
-          async function urlGet(url){
-              let axios=require('axios').default
-              axios.get(url)
-            .then(function (response) {
-              // handle success
-              let c=message.guild.channels.cache.get(ch)
-              c.send(response.data.message)
-            })
-            .catch(function (error) {
-              // handle error
-              console.log(error);
-            })
-            .then(function () {
-            });
-              
-          }
-          urlGet(`https://api.udit.tk/api/chatbot?message=${message.content}`)
-          return
-          
-      }}
+import axios from "axios";
+import { Message, TextChannel } from "discord.js";
+import chatBot from "../Schemas/chatbot";
+async function check(message: Message) {
+  let check = await chatBot.findOne({ guildID: message.guild?.id });
+  if (check) {
+    let ch = check.channelID;
+    if (message.channel.id === ch) {
+      async function urlGet(url: string) {
+        let axios = require("axios").default;
+        axios
+          .get(url)
+          .then(function (response: any) {
+            // handle success
+            let c = message.guild?.channels.cache.get(ch) as TextChannel;
+            c.send(response.data.message);
+          })
+          .catch(function (error: any) {
+            // handle error
+            console.log(error);
+          })
+          .then(function () {});
+      }
+      urlGet(`https://api.udit.tk/api/chatbot?message=${message.content}`);
+      return;
     }
-export default check
+  }
+}
+export default check;
