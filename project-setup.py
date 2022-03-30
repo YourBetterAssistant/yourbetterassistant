7e1aaa202679e5090122e6be1c6adf6b12a9dcda
NotTimIsReal <84319818+NotTimIsReal@users.noreply.github.com>
2022-03-30 18:08:41 +1100

    Update project-setup.py

--- project-setup.py
+++ project-setup.py
@@ -1,5 +1,12 @@
 from os import strerror
 import subprocess
-subprocess.call(["yarn", "-v"], stderr=quit(1), stdout=subprocess.DEVNULL)
-subprocess.call(["yarn", "install"], stdout=subprocess.DEVNULL)
+#quit if stderr
+cmd= subprocess.getstatusoutput("yarn -v")
+if cmd[0] != 0:
+    print("Yarn Is Not Installed")
+    quit(1)
+status=subprocess.call(["yarn", "ee"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+if status != 0:
+    print("Yarn Error")
+    quit(1)
 print("setup.project.status.sucess")
